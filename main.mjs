b1c6299587c0a40a59020942a6f2ca6b106623c8
Michael Wilke <mwilke@enarchtech.com>
2021-12-18 17:42:44 -0600

    Removed Auth from Main File. Moved to authhandler

--- main.mjs
+++ main.mjs
@@ -1,79 +1,20 @@
-import { StaticAuthProvider } from '@twurple/auth';
-import { ApiClient } from '@twurple/api';
-import { ChatClient } from '@twurple/chat';
-import { PubSubClient } from '@twurple/pubsub';
+import { NgrokAdapter } from '@twurple/eventsub-ngrok';
+import { EventSubListener } from '@twurple/eventsub';
 import { followAgeListener } from './commands/twitch/followage.mjs';
-import * as axios from 'axios';
+import { ttvchatClient, discordClient } from './authhandler.mjs';
 import * as fs from 'fs';
-//import { client } from 'websocket'; //TODO
-//import { WebSocketServer } from 'w';
-import { Client, Collection, MessageEmbed } from 'discord.js';
-//import { Chatbox, ChatboxMessage } from './modules/chatBox.js'
+import { Collection, MessageEmbed } from 'discord.js';
 
 import dotenv from 'dotenv';
-import { channel } from 'diagnostics_channel';
-//import { FollowerAlertSequance, setLightsForStream, setLightsToRandomColors, controlAllLights } from './hue.mjs';
 dotenv.config();
 
-//Hue
-/*const HUEBRIDGEIP = process.env.bridgeIP;
-const HUEUSER = process.env.hueUsername;
 
-const controlLight = async(lightId, on, hue, sat, bri) => {
-	try {
-		return await axios.put(
-			`http://${HUEBRIDGEIP}/api/${HUEUSER}/lights/${lightId}/state`,
-			{
-				on,
-					... (sat && { sat}),
-					... (bri && { bri }),
-					... (hue && { hue }),
-			}
-		);
-	}catch (err) {
-		//console.error(error);
-	}
-};*/
-
-//create auth const
-const ttvClientId = process.env.ttwClientId;
-const ttvClientSecret = process.env.ttvClientSecret;
-const ttvAccessToken = process.env.ttvClientTokenAcc;
-const ttvRefreshtoken = process.env.ttvRefreshtoken;
 const ttvEventLog = process.env.discordTTVLogChannel;
 
-const discordToken = process.env.discordToken;
 const discordClientId = process.env.discordClientId;
 const discordGuildId = process.env.discordGuildId;
-//const commandFilesTwitch = fs.readdirSync("commands/twitch").filter(file => file.endsWith(".js"));
 
-
-//auth twitch
-const authProvider = new StaticAuthProvider(ttvClientId, ttvAccessToken);
-
-
-const apiClient = new ApiClient({ authProvider });
-const ttvpubSubClient = new PubSubClient();
-const userId = await ttvpubSubClient.registerUserListener(authProvider);
-const ttvchatClient = new ChatClient({ authProvider, channels: [process.env.ttvChannel] });
-const discordClient = new Client ({
-	intents: ["GUILDS", "GUILD_MESSAGES"],
-});
 const discordbotlog = discordClient.channels.cache.get(ttvEventLog);
-
-
-//connect to chat
-await ttvchatClient.connect();
-ttvchatClient.onRegister((channel, msg) => {
-    console.log('TTVconnected');
-});
-
-discordClient.once('ready', () => {
-	console.log('Discord Connected');
-	discordClient.user.setPresence({ activities: [{ name: 'Tending to the Fire', type: 'STREAMING', url: "https://twitch.tv/agent_flame" }], status: 'live' });
-});
-discordClient.login(discordToken);
-
 
 //start of command handler
 discordClient.commands = new Collection();
@@ -91,39 +32,53 @@ for (const file of commandFiles) {
 ttvchatClient.onMessage((channel, user, message) => {
 	if (message === '!ping') {
 		ttvchatClient.say(channel, 'Pong!');
+		console.log(`@${user} ran command !ping`);
 	} else if (message === '!dice') {
 		const diceRoll = Math.floor(Math.random() * 6) + 1;
 		ttvchatClient.say(channel, `@${user} rolled a ${diceRoll}`)
+		console.log(`@${user} ran command !dice`);
 	} else if (message === '!costream') {
 		ttvchatClient.say(channel, 'Tonight we are surviving in the forest together. Joining me we have Askesienne http://twitch.tv/askesienne and Lantheos http://twitch.tv/lantheos. Go check them out and say Hi! You can see all of our views here: https://multistre.am/agent_flame/askesienne/lantheos/layout7/');
+		console.log(` @${user} ran command !costream`);
 	} else if (message === '!pronouns') {
 		ttvchatClient.say(channel, '/me I use they/them! If you would like to be able to set your pronouns on Twitch, I will be able to see them directly in chat using a browser extension. Your pronouns will show up next to your name for anyone to read! Log in with Twitch at https://pronouns.alejo.io/ to adjust your settings. <3');
+		console.log(` @${user} ran command !pronouns`);
 	} else if (message === '!hello') {
 		ttvchatClient.say(channel, `Heya! @${user} My hame is Campfirebot! Nice to meet you!`);
+		console.log(` @${user} ran command !hello`);
 	} else if (message === '!discord') {
 		ttvchatClient.say(channel, 'Join us on Discord for in-game voice, going live alerts, and other chit-chat! https://discord.gg/TbUtUjY');
+		console.log(` @${user} ran command !discord`);
 	} else if (message === '!stjude') {
 		ttvchatClient.say(channel, "This month we are fundraising for St. Jude Children's Research Hospital. It is St. Jude's mission to provide treatment and care to all regardless of race, religion. As well as never leave the family with a bill. https://tiltify.com/@agent_flame/the-campfire-x-st-jude-play-live-2021");
+		console.log(` @${user} ran command !stjude`);
 	} else if (message === '!zoomzoom') {
 		ttvchatClient.say(channel, 'Lets !Race');
+		console.log(` @${user} ran command !zoomzoom`);
 	} else if (message === '!lurk') {
 		ttvchatClient.say(channel, `@${user} has decided to minimize instead of quit! Catch you later!`);
+		console.log(` @${user} ran command !lurk`);
 	} else if (message === '!zoomzoom2') {
 		ttvchatClient.say(channel, '!boost');
+		console.log(` @${user} ran command !zoomzoom2`);
 	} else if (message === '!unlurk') {
 		ttvchatClient.say(channel, `Welcome Back @${user}`);
+		console.log(` @${user} ran command !unlurk`);
 	} else if (message === '!commands') {
 		ttvchatClient.say(channel, '!ping, !dice, !costream, !pronouns, !hello, !discord, !stjude, !zoomzoom, !lurk, !zoomzoom2, !unlurk, !commands');
-	}
-	else if (message === '!systest') {
+		console.log(`@${user} ran command !commands`);
+	} else if (message === '!systest') {
 		discordClient.channels.cache.get(ttvEventLog).send(`@${user} is testing`);
 		ttvchatClient.say(channel, 'Main Bot Test Sent');
 		console.log("testing main");
-		//FollowerAlertSequance();
-		//SubAlertSequance();
+		console.log(`@${user} ran command !systest`);
+	} else if (message === '!raidcall') {
+		ttvchatClient.say(channel, `"/me RAID FROM THE CAMPFIRE"`);
+		console.log(`@${user} ran command !raidcall`);
 	}
-	else if (message == '!bing') {
+	else if (message === '!bing') {
 		ttvchatClient.say(channel, 'BING BONG!');
+		console.log(`@${user} ran command !bing`);
 	}
 	else if (message === 'hey') {
 		ttvchatClient.say(channel, `hello @${user}`);
@@ -140,14 +95,12 @@ ttvchatClient.onMessage((channel, user, message) => {
 ttvchatClient.onSub((channel, user) => {
 	ttvchatClient.say(channel, `Welcome around the campfire @${user}!`);
 	discordClient.channels.cache.get(ttvEventLog).send(`@${user} just subscribed`);
-	//FollowerAlertSequance();
 
 });
 
 ttvchatClient.onResub((channel, user, subInfo) => {
 	ttvchatClient.say(channel, `Thanks to @${user} for subscribing to the channel for a total of ${subInfo.months} months!`);
 	discordClient.channels.cache.get(ttvEventLog).send(`@${user} just resubscribed`);
-	//FollowerAlertSequance();
 });
 
 ttvchatClient.onSubGift((channel, user, subInfo) => {
@@ -155,18 +108,4 @@ ttvchatClient.onSubGift((channel, user, subInfo) => {
 	discordClient.channels.cache.get(ttvEventLog).send(`@${subInfo.gifter} just gifed a subscription to ${user}!`);
 });
 
-
-/*//Listening for PubSub Sub event
-const listener = await ttvpubSubClient.onSubscription(userId, (message) => {
-    console.log(`${message.userDisplayName} just subscribed!`);
-});*/
-export { ttvchatClient, apiClient };
-
-/*
-async function isStreamLive(userName: string) {
-	const user = await apiClient.users.getUserByName(userName);
-	if (!user) {
-		return false;
-	}
-	return await user.getStream() !== null;
-}*/
+export { };
