4071908ed6751f9bbd5a1e7a6aa611733c9f98bf
Michael Wilke <mwilke@enarchtech.com>
2022-02-02 04:19:45 -0600

    Fixed Anon Auth Issue

---
+++ ttvEventSub.mjs
@@ -0,0 +1,23 @@
+import { NgrokAdapter } from "@twurple/eventsub-ngrok";
+import { EventSubListener } from '@twurple/eventsub';
+import { ApiClient } from '@twurple/api';
+import { ClientCredentialsAuthProvider } from "@twurple/auth";
+
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const clientId = process.env.ttvClientId;
+const clientSecret = process.env.ttvClientSecret
+
+const apiAuth = new ClientCredentialsAuthProvider(clientId, clientSecret);
+const apiClient = new ApiClient({authProvider: apiAuth})
+const eventListener = new EventSubListener({
+    apiClient,
+    adapter: new NgrokAdapter,
+    secret: 'TheCampFire'
+});
+
+export {
+    eventListener
+}
---
+++ ttvPubSubAuth.mjs
@@ -0,0 +1,12 @@
+import { PubSubClient } from "@twurple/pubsub";
+import { authProvider } from "./ttvChatAuth.mjs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const tvPubSubClient = new PubSubClient();
+const userId = await tvPubSubClient.registerUserListener(authProvider);
+
+export {
+    tvPubSubClient
+}
