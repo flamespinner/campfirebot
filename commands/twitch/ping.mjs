6b1d904f5e26dc57c67625991f1539f6b6c2a65a
Michael Wilke <mwilke@enarchtech.com>
2022-05-16 12:34:04 -0500

    Ping Command Work

--- commands/twitch/ping.mjs
+++ commands/twitch/ping.mjs
@@ -2,8 +2,21 @@ import { ttvchatClient } from '../../authhandler.mjs';
 import process from 'process';
 
 async function ping() {
-    ttvchatClient.say(`Agent_Flame`, `Pong! The CampFire has burning for ${process.uptime()} seconds`);
+    ttvchatClient.say(`Agent_Flame`, `Pong! The CampFire has burning for ${format(uptime)}, ${uptime}, ${process.uptime()}`);
 }
+
+function format(seconds){
+    function pad(s){
+      return (s < 10 ? '0' : '') + s;
+    }
+    var hours = Math.floor(seconds / (60*60));
+    var minutes = Math.floor(seconds % (60*60) / 60);
+    var seconds = Math.floor(seconds % 60);
+  
+    return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+  }
+  
+  var uptime = process.uptime();
 
 export {
     ping
