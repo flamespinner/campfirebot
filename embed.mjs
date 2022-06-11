f87a98fb36d0c20317b90c3f1783e366b13bebdc
Michael Wilke <repair@wilkerepair.com>
2022-06-11 10:07:52 -0500

    Added Embed Formatting

--- embed.mjs
+++ embed.mjs
@@ -1,33 +1,46 @@
-import { discordClient } from './authhandler.mjs';
+import { discordClient, ttvchatClient } from './authhandler.mjs';
 import { MessageEmbed } from 'discord.js';
 
-const ttvEventLog = process.env.discordTTVLogChannel;
+const ttvEventLog = process.env.discordTestChannel;
 const channel = discordClient.channels.cache.get(ttvEventLog);
 
-// inside a command, event listener, etc.
-const exampleEmbed = new MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+const followEmbed = new MessageEmbed()
+	.setColor('#00FF00')
+	.setTitle('New Follower!')
 	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setThumbnail('https://raw.githubusercontent.com/PhantomBot/Miscellaneous/master/Discord-Embed-Icons/follow-embed-icon.png')
 	.setTimestamp()
-	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+	.setFooter({ text: 'Twitch'});
+channel.send({ embeds: [followEmbed] });
 
-channel.send({ embeds: [exampleEmbed] });
 
-export { exampleEmbed }
+const hostEmbed = new MessageEmbed()
+	.setColor('#ff0000')
+	.setTitle('New Host!')
+	.setDescription('Some description here')
+	.setThumbnail('https://raw.githubusercontent.com/PhantomBot/Miscellaneous/master/Discord-Embed-Icons/host-embed-icon.png')
+	.setTimestamp()
+	.setFooter({ text: 'Twitch'});
+channel.send({ embeds: [hostEmbed] });
 
-//follow icon: https://raw.githubusercontent.com/PhantomBot/Miscellaneous/master/Discord-Embed-Icons/follow-embed-icon.png
-//new host: https://raw.githubusercontent.com/PhantomBot/Miscellaneous/master/Discord-Embed-Icons/host-embed-icon.png
-//new sub: https://static-cdn.jtvnw.net/badges/v1/5d9f2208-5dd8-11e7-8513-2ff4adfae661/2
-//bit https://d3aqoihi2n8ty8.cloudfront.net/actions/cheer/dark/animated/100/1.gif
+
+const subEmbed = new MessageEmbed()
+	.setColor('#6441a5')
+	.setTitle('New Subscriber!')
+	.setDescription('Some description here')
+	.setThumbnail('https://static-cdn.jtvnw.net/badges/v1/5d9f2208-5dd8-11e7-8513-2ff4adfae661/2')
+	.setTimestamp()
+	.setFooter({ text: 'Twitch'});
+channel.send({ embeds: [subEmbed] });
+
+
+const bitEmbed = new MessageEmbed()
+	.setColor('#6441a5')
+	.setTitle('New Cheer!')
+	.setDescription('Some description here')
+	.setThumbnail('https://d3aqoihi2n8ty8.cloudfront.net/actions/cheer/dark/animated/100/1.gif')
+	.setTimestamp()
+	.setFooter({ text: 'Twitch'	});
+channel.send({ embeds: [bitEmbed] });
+
+export { followEmbed, hostEmbed, subEmbed, bitEmbed }
